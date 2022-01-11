fcc0c59aea3f4ce5cb55d3ea420eeb15add8d4c3
Kuliiev Roman <Mejas.black@gmail.com>
2022-01-11 21:09:25 +0200

    finished

---
+++ src/views/contactsView/ContactsViewStyled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const ContactsViewContainer = styled.div`
+  h2 {
+    padding: 1vh 0 0.5vh;
+    color: darkgray;
+    font-weight: 700;
+  }
+`;
---
+++ src/views/homeView/HomeViewStyled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const HomeViewContainer = styled.div`
+  padding: 4vw 2vw 0;
+  font-size: 2vw;
+  color: darkgray;
+  text-align: center;
+  font-weight: 700;
+`;
